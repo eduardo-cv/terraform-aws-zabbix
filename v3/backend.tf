0ff1775783ec9487b71ee750425f8e3a01ca384a
Alves-SP <82802634+eduardo-cv-developer@users.noreply.github.com>
2023-03-05 20:38:59 -0300

    Delete v3 directory

--- v3/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-state-zabbix" # Trocar aqui
-    key    = "terraform.tfstate"
-    region = "us-east-1"
-  }
-}
--- v3/files-user-data/user-data-zabbix.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-wget https://repo.zabbix.com/zabbix/6.2/ubuntu/pool/main/z/zabbix-release/zabbix-release_6.2-4%2Bubuntu22.04_all.deb
-sudo dpkg -i zabbix-release_6.2-4+ubuntu22.04_all.deb
-sudo apt-get update -y
-sudo apt-get install zabbix-server-mysql zabbix-frontend-php zabbix-nginx-conf zabbix-sql-scripts zabbix-agent2 -y
-sudo systemctl restart zabbix-server zabbix-agent2 nginx php8.1-fpm
-sudo systemctl enable zabbix-server zabbix-agent2 nginx php8.1-fpm
